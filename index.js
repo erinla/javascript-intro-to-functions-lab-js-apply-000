f4cff2055fe998b68403524090b2967a811156c9
Erin Lawler <erin_lawler@yahoo.com>
2019-03-06 21:50:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function sayHiToGrandma(string) {
   var lowercase = "I can't hear you!"
   var uppercase = "YES INDEED!"
   var love = "I love you, too."
-  if (string.toLowerCase() === lowercase) {
+  if (string.toLowerCase(string) === lowercase) {
     return lowercase
   }
-  else if (string.toUpperCase() === uppercase) {
+  else if (string.toUpperCase(string) === uppercase) {
     return uppercase
   }
   else if (string === "I love you, Grandma.") {
