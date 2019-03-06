9a9d12e0cf853da1a8d4e646177988930ed21ed9
Erin Lawler <erin_lawler@yahoo.com>
2019-03-06 21:20:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  return string.toLowerCase === "I can't hear you!"
-  return string.toUpperCase === "YES INDEED"
+  return string.toLowerCase(string) === "I can't hear you!"
+  return string.toUpperCase(string) === "YES INDEED"
 }
