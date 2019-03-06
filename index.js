b01362f7b00de8993eac10b8cf97df939871a94c
Erin Lawler <erin_lawler@yahoo.com>
2019-03-06 21:51:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  var lowercase = "I can't hear you!"
-  var uppercase = "YES INDEED!"
+  var hear = "I can't hear you!"
+  var indeed = "YES INDEED!"
   var love = "I love you, too."
-  if (string.toLowerCase(string) === lowercase) {
-    return lowercase
+  if (string.toLowerCase() === lowercase) {
+    return hear
   }
-  else if (string.toUpperCase(string) === uppercase) {
-    return uppercase
+  else if (string.toUpperCase() === uppercase) {
+    return indeed
   }
   else if (string === "I love you, Grandma.") {
     return love
