c2cadf811823c848a3d2b1db7ab798cdf9cba02a
Erin Lawler <erin_lawler@yahoo.com>
2019-03-06 21:03:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function logShout(string) {
 function logWhisper(string) {
   console.log() string.toLowerCase()
 }
-funtion sayHiToGrandma(string) {
-  
+function sayHiToGrandma(string) {
+  return string.
 }
